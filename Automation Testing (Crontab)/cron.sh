c7a3ceb54046c38bdb12821da2e026d712be61be
Hanif Aditia Sofian <121953045+hanifaditia@users.noreply.github.com>
2025-05-04 13:47:19 +0700

    Update cron.sh

--- Automation Testing (Crontab)/cron.sh
+++ Automation Testing (Crontab)/cron.sh
@@ -1,3 +1,3 @@
 # ADD THIS SCRIPT TO CRONTAB
-0 8,12,15 * * * /usr/bin/python3 ./scrapping.py 
-0 0 * * * /bin/bash ./data_cleansing.sh
+0 8,12,15 * * * /usr/bin/python3 ./scrapping.py #“./scrapping.py” can be change to path to scrapping.py
+0 0 * * * /bin/bash ./data_cleansing.sh #“./data_cleansing.sh” can be change to path to data_cleansing.sh
